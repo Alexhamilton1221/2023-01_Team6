ba55dda5114e6861aa171da5f47966f29e88907f
Ecars9 <ecarstensen99@gmail.com>
2023-02-01 20:33:21 -0700

    enhanced excel reading capabilities

---
+++ classes.py
@@ -0,0 +1,16 @@
+
+
+class Class:
+    def __init__(self, id):
+        self.id = id
+        self.desc = ""
+        self.hours = 0
+        self.room = ""
+        self.type = ""
+        self.term = 0
+
+
+    def __str__(self):
+
+        return str([self.id, self.desc, self.hours, self.term, self.type])
+        
