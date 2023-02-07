d6b01c87fd06eefb8b933b1313defd02aa0a768d
Conlan2 <conlan2222@gmail.com>
2023-02-07 16:29:25 -0700

    added hard coded classrooms to git

---
+++ hardCodedClassrooms.py
@@ -0,0 +1,14 @@
+from Database.classroom import Classroom
+def temp_Classroom_add():
+    # This is a temp function to get the classrooms into a list
+    r533 = Classroom("11-533", 36)
+    r534 = Classroom("11-534", 36)
+    r560 = Classroom("11-560", 24)
+    r562 = Classroom("11-562", 24)
+    r564 = Classroom("11-564", 24)
+    r458 = Classroom("11-458", 40)
+    r430 = Classroom("11-430", 30)
+    r320 = Classroom("11-320", 30)
+    r532 = Classroom("11-532 Computer Lab", 30, True)
+
+    return [r533, r534, r560, r562, r564, r458, r430, r320, r532]
