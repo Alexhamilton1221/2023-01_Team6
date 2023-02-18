c438bb9b2778a928a07fc1b813bb43dfa85f7d72
Conlan2 <conlan2222@gmail.com>
2023-02-18 10:45:08 -0700

    Refactored code to move duplicate commands into functions in cohorts and classrooms for better readablity

--- Database/cohorts.py
+++ Database/cohorts.py
@@ -1,3 +1,6 @@
+import math
+
+from Database.classrooms import Classrooms
 from Database.cohort import Cohort
 from calc_space import get_hours
 from calc_space import get_FullStack_hours
@@ -9,6 +12,18 @@ class Cohorts:
         # The cohorts of the database (array of cohort)
         self.cohorts = cohorts
 
+    @staticmethod
+    def __read_students__(students):
+        # Turns a array of students with the format [("PCOM 1", 54) -> ["PCOM", 1, 54] and sorts it
+        # format is [program, term, students]
+        termed_students = []
+        for groups in students:
+            group = groups[0].split()
+            termed_students.append([group[0], int(group[1]), groups[2]])
+
+        termed_students.sort(key=lambda program_count: program_count[2], reverse=True)
+        return termed_students
+
     def create_cohorts(self, classrooms, programs, students):
         # Given an amount of classrooms in the classrooms object
         # programs from the programs object
@@ -16,21 +31,9 @@ class Cohorts:
         # Ex: [("PCOM 1", 54),("PCOM 2", 45), ("PCOM 3", 81), ("PC 2", 32)]
 
         # This is sorting the list of students from largest to smallest amount of students
-        students.sort(key=lambda program_count: program_count[1], reverse=True)
-
-        # This turns the input into the program name and term in the correct format
-        # which is [program, term, students]
-        termed_students = []
-        for groups in students:
-            group = groups[0].split()
-            termed_students.append([group[0], int(group[1]), groups[2]])
-
-
+        termed_students = self.__read_students__(students)
         # These are the sorted lists of classrooms by their size from largest to smallest
-        classes_by_size = classrooms.get_rooms(lambda x: x.is_lab == False)
-        labs_by_size = classrooms.get_rooms(lambda x: x.is_lab == True)
-        classes_by_size.sort(key=lambda x: x.size, reverse=True)
-        labs_by_size.sort(key=lambda x: x.size, reverse=True)
+        classes_by_size, labs_by_size = Classrooms.rooms_by_size(classrooms)
 
         # These are lists of all the classrooms sizes by themselves, Ex [40, 36, 30, 24]
         class_sizes = []
@@ -50,12 +53,11 @@ class Cohorts:
         # This creates a separate list containing the lab hours for core (left) and program (right), with extra hours
         # that full stack classes get on the far right.
         # As cohorts are assigned the amount of spare hours will be decreased
-        for room in classes_by_size:
+        for room in labs_by_size:
             # FORMAT [ROOM, CORE HOURS, PROGRAM HOURS, FULL STACK ONLY HOURS]
             lab_hours.append([room, get_hours(), get_hours(), get_FullStack_hours()])
             if room.size not in class_sizes:
-                class_sizes.append(room.size)
-
+                lab_sizes.append(room.size)
 
         # This goes through all grouping of students
         for unsorted_students in termed_students:
@@ -64,8 +66,13 @@ class Cohorts:
             while not split:
                 # Goes for all sizes of classrooms in class_sizes
                 for size in class_sizes:
+                    # Multiplies by 0.9 to leave extra room inside cohorts
+                    cohort_count = math.ceil((unsorted_students[2] / (size * 0.9)))
 
-
+                    # This gets how many hours of coursework will be needed by the course, and gets them for only
+                    # classes in classrooms
+                    class_hours = programs.get_program(lambda program: program.name == unsorted_students[0]).get_hours(
+                        lambda course: not course.is_lab())
 
 
 
