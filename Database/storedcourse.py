e1ad636bddcd5e4b0ccce43138f8189d2f9b18a0
Brian Lin <brian.lin.blin@gmail.com>
2023-03-02 16:23:42 -0700

    Added lecture_length and number_of_lecture class functions to storedcourse class
    also added import re to stored course, not sure if there's a better place for that or not

--- Database/storedcourse.py
+++ Database/storedcourse.py
@@ -1,4 +1,6 @@
 from Database.course import Course
+import re
+
 class StoredCourse:
     # This is the long term stored info about the course
     def __init__(self, name, total_hours, term, prerequisites, delivery = "Class", extra_req = ""):
@@ -24,3 +26,17 @@ class StoredCourse:
         for course in self.prerequisites:
             prerequisites.append(course.generate_course())
         return Course(self.name, self.total_hours, prerequisites, self.delivery)
+
+    #returns value for lecture length from extra_req value from storedcourse object
+    def lecture_length(self):
+        length_of_lecture = self.extra_req
+        #Brian: not all stored lectures have a lecture length, not sure what to do when that happens
+        if length_of_lecture[0] == "H":
+            #checking if extra req has H
+            length = re.search('=(.+?)h',length_of_lecture)
+            return float(length.group(1))
+        else:
+            return None
+    def number_of_lectures(self):
+        number_of_lectures = self.total_hours / self.lecture_length()
+        return number_of_lectures
