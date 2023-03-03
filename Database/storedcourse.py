f68b0df20aa2e212b9bd8d53dc3aea4ab78cbe64
Conlan2 <conlan2222@gmail.com>
2023-03-02 20:56:17 -0700

    Added comment description to stored course

--- Database/storedcourse.py
+++ Database/storedcourse.py
@@ -1,6 +1,9 @@
 from Database.course import Course
 class StoredCourse:
     # This is the long term stored info about the course
+    # !!! This is NOT the object within the cohort !!!
+    # This is the object within the program that is used to create each course!
+    # THis object is effectively a template for the actual course and should never appear within a cohort object
     def __init__(self, name, total_hours, term, prerequisites, delivery = "Class", extra_req = ""):
         # The name of the course (String)
         self.name = name
