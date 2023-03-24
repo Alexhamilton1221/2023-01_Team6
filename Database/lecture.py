b7bd8fff445a4c32f2a0b21bac4948867157d40e
Conlan2 <conlan2222@gmail.com>
2023-03-24 15:50:57 -0600

    made an abomination of a lecutre time comparision function to try to prevent all overlaps of courses

--- Database/lecture.py
+++ Database/lecture.py
@@ -22,5 +22,24 @@ class Lecture:
                 return True
         return False
 
+    def time_collison_compare(self, start_time, end_time):
+        # Purpose: This function checks if the time, but not the date of another lecture is in collision with tihs
+        startLessThanStart = self.start_time < start_time
+        endLessThanEnd = self.end_time < end_time
+        startLessThanEnd = self.start_time < end_time
+        endGreaterThanStart = self.end_time > start_time
+
+        if startLessThanStart and endLessThanEnd and startLessThanEnd and endGreaterThanStart:
+            return True
+        elif not startLessThanStart and not endLessThanEnd and startLessThanEnd and endGreaterThanStart:
+            return True
+        elif startLessThanStart and not endLessThanEnd and startLessThanEnd and endGreaterThanStart:
+            return True
+        elif not startLessThanStart and endLessThanEnd and startLessThanEnd and endGreaterThanStart:
+            return True
+
+        return False
+
+
     def length(self):
         return self.end_time - self.start_time
