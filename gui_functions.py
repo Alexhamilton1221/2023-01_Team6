51aaa34d8b9880660fa2cad40a3cbca009bc6b87
Conlan2 <conlan2222@gmail.com>
2023-03-28 17:31:02 -0600

    Fixed january spelling mistake

--- gui_functions.py
+++ gui_functions.py
@@ -1092,7 +1092,7 @@ def date_suffix(date):
     return suffix_dict[date]
 
 def month_num_to_name(num):
-    months = {	'1':'Janauary',
+    months = {	'1':'January',
 		'2':'February',
 		'3':'March',
 		'4':'April',
